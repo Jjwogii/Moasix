0bc3cbbcbf6ffc6376ed0108a91087cebe48db80
Warren D. Rezane <warzkie123@gmail.com>
2023-02-18 12:56:17 +0800

    Replaced intents and partials.

--- src/bot.js
+++ src/bot.js
@@ -1,37 +1,63 @@
 const { Player } = require("discord-player");
-const { Client, Collection } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
 const fs = require("node:fs");
 const yaml = require("js-yaml");
 
 if (!fs.existsSync("config.yml")) {
-    return console.error("[Aborted] Unable to find config.yml file. Please copy the default configuration into a file named config.yml in the root directory. (The same directory as package.json)");
+  return console.error(
+    "[Aborted] Unable to find config.yml file. Please copy the default configuration into a file named config.yml in the root directory. (The same directory as package.json)"
+  );
 }
 
 if (!fs.existsSync("src/data.json")) {
-    fs.writeFileSync("src/data.json", JSON.stringify({ "songs-played": 0, "queues-shuffled": 0, "songs-skipped": 0 }));
+  fs.writeFileSync(
+    "src/data.json",
+    JSON.stringify({ "songs-played": 0, "queues-shuffled": 0, "songs-skipped": 0 })
+  );
 }
 
 const configFile = yaml.load(fs.readFileSync("./config.yml"));
 
 global.config = {
-    token: configFile.botToken ?? "",
-    clientId: configFile.clientId ?? "",
-    geniusKey: configFile.geniusApiKey ?? null,
-    embedColour: configFile.embedColour ?? "#2F3136",
-    analytics: configFile.enableAnalytics ?? true,
-    stopEmoji: configFile.emojis.stop ?? "⏹",
-    skipEmoji: configFile.emojis.skip ?? "⏭",
-    queueEmoji: configFile.emojis.queue ?? "📜",
-    pauseEmoji: configFile.emojis.pause ?? "⏯",
-    lyricsEmoji: configFile.emojis.lyrics ?? "📜",
-    backEmoji: configFile.emojis.back ?? "⏮",
+  token: configFile.botToken ?? "",
+  clientId: configFile.clientId ?? "",
+  geniusKey: configFile.geniusApiKey ?? null,
+  embedColour: configFile.embedColour ?? "#2F3136",
+  analytics: configFile.enableAnalytics ?? true,
+  stopEmoji: configFile.emojis.stop ?? "⏹",
+  skipEmoji: configFile.emojis.skip ?? "⏭",
+  queueEmoji: configFile.emojis.queue ?? "📜",
+  pauseEmoji: configFile.emojis.pause ?? "⏯",
+  lyricsEmoji: configFile.emojis.lyrics ?? "📜",
+  backEmoji: configFile.emojis.back ?? "⏮",
 };
 
-if (!global.config.token || global.config.token === "") return console.error("[Aborted] Please supply a bot token in your configuration file.");
-if (!global.config.clientId || global.config.clientId === "") return console.error("[Aborted] Please supply a client ID in your configuration file.");
+if (!global.config.token || global.config.token === "")
+  return console.error("[Aborted] Please supply a bot token in your configuration file.");
+if (!global.config.clientId || global.config.clientId === "")
+  return console.error("[Aborted] Please supply a client ID in your configuration file.");
 if (global.config.geniusKey === "") global.config.geniusKey = null;
 
-const client = new Client({ intents: [32767] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User,
+  ],
+});
 global.player = new Player(client);
 client.commands = new Collection();
 client.buttons = new Collection();
@@ -39,11 +65,11 @@ client.buttons = new Collection();
 const functions = fs.readdirSync("./src/functions").filter((file) => file.endsWith(".js"));
 
 (async () => {
-    for (var file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handleCommands();
-    client.handleEvents();
-    client.handleButtons();
-    client.login(global.config.token);
+  for (var file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+  client.handleCommands();
+  client.handleEvents();
+  client.handleButtons();
+  client.login(global.config.token);
 })();
