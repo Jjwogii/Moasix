eeec957e45e7a789778bf9b4a65441ef3f86b5ea
Warren D. Rezane <warzkie123@gmail.com>
2023-02-19 13:53:07 +0800

    Re-formatted code.

--- src/events/bot/messageCreate.js
+++ src/events/bot/messageCreate.js
@@ -2,9 +2,7 @@ const yaml = require("js-yaml");
 const fs = require("node:fs");
 
 if (!fs.existsSync("config.yml")) {
-  return console.error(
-    "[Aborted] Unable to find config.yml file. Please copy the default configuration into a file named config.yml in the root directory. (The same directory as package.json)"
-  );
+    return console.error("[Aborted] Unable to find config.yml file. Please copy the default configuration into a file named config.yml in the root directory. (The same directory as package.json)");
 }
 
 const configFile = yaml.load(fs.readFileSync("./config.yml"));
@@ -12,30 +10,30 @@ const configFile = yaml.load(fs.readFileSync("./config.yml"));
 const commands = require("../../commands-metadata.json");
 
 module.exports = {
-  name: "messageCreate",
-  once: false,
+    name: "messageCreate",
+    once: false,
 
-  /**
-   * @param {import('discord.js').Message} message
-   * @returns {void}
-   */
-  async execute(message) {
-    if (message.author.bot) return;
-    if (message.content.charAt(0) !== configFile.prefix) return;
+    /**
+     * @param {import('discord.js').Message} message
+     * @returns {void}
+     */
+    async execute(message) {
+        if (message.author.bot) return;
+        if (message.content.charAt(0) !== configFile.prefix) return;
 
-    // Transform Interaction instance to Message.
-    // This is to basically trick the slash command.
-    const interaction = message;
+        // Transform Interaction instance to Message.
+        // This is to basically trick the slash command.
+        const interaction = message;
 
-    // Get the command
-    const command = message.content.split(" ").shift().slice(1);
-    const metadata = commands.find((c) => c.command === command || c.aliases.includes(command));
+        // Get the command
+        const command = message.content.split(" ").shift().slice(1);
+        const metadata = commands.find((c) => c.command === command || c.aliases.includes(command));
 
-    // Return if metadata is undefined
-    if (typeof metadata === "undefined") return;
+        // Return if metadata is undefined
+        if (typeof metadata === "undefined") return;
 
-    // Run the slash command using prefix
-    // Tricky, but it works lol.
-    require(`../../commands/${metadata.folder}/${metadata.command}`).execute(interaction);
-  },
+        // Run the slash command using prefix
+        // Tricky, but it works lol.
+        require(`../../commands/${metadata.folder}/${metadata.command}`).execute(interaction);
+    },
 };
